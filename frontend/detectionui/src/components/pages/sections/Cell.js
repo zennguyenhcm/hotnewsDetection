4c03fbb8bc522ad928f65ee578be2d79c14973b4
zennguyenhcm <zennguyen.hcm@gmail.com>
2020-01-25 22:41:16 +0700

    add truncate function to shorten long string

--- frontend/detectionui/src/components/pages/sections/Cell.js
+++ frontend/detectionui/src/components/pages/sections/Cell.js
@@ -1,6 +1,24 @@
 import * as React from 'react';
 import styles from '../styles.module.css';
-
+const text_truncate = (str, length, endings) => {
+  console.log ('hello_truncate');
+  if (length == null) {
+    length = 100;
+  }
+  if (endings == null) {
+    endings = '...';
+  }
+  if (str.length > length) {
+    console.log ('string is too long: ');
+    let shorted_str = str.substr (0, length - endings.length) + endings;
+    console.log ('string is too long: ', shorted_str);
+    return shorted_str;
+  } else {
+    console.log ('lenght', str.length);
+    console.log ('string is not too long');
+    return str;
+  }
+};
 export default function Cell({content, header}) {
   const state = {showForm: false};
 
@@ -9,7 +27,7 @@ export default function Cell({content, header}) {
         {content}
       </th>
     : <td>
-        {content}
+        {text_truncate (content.toString (), 50)}
       </td>;
 
   return cellMarkup;
